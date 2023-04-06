c987b4074fdf5458a79aaf70e4631ae25640873d
C0iAS <sebapl09@gmail.com>
2023-04-06 18:57:21 -0500

    styles, db

---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Category } from '../Category'
+import { List, Item } from './styles.js'
+import db from '../../../api/db.json'
+
+export const ListOfCategories = () => {
+  return (
+    <List>
+      {
+        db.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App'
 
 const rootElement = document.getElementById('app')
 const root = createRoot(rootElement)
-root.render(<h1>'Hola coias'</h1>)
+root.render(
+  <App/>
+  )
