48d9167c7a88974a6cc42dab5952e074bcc828cd
coias <sebapl09@gmail.com>
2023-04-09 10:38:29 -0500

    list of categories update

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles.js'
+import { MdHourglassEmpty } from "react-icons/md";
 
-export const ListOfCategories = () => {
+function useCategoriesData() {
   const [categories, setCategories] = useState([])
+  const [loading, setloading] = useState(false)
 
   useEffect(function () {
+    setloading(true)
     window.fetch('https://petgram-server-coias-c0ias.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
+        setloading(false)
       })
   }, [])
+  return { categories, loading }
+}
 
-  return (
-    <List>
+export const ListOfCategories = () => {
+    const { categories, loading } = useCategoriesData()
+    const [showFixed, setShowFixed] = useState(false)
+
+    useEffect(function() {
+      const onScroll = e => {
+        const newShowFixed = window.scrollY > 200
+        showFixed !== newShowFixed && setShowFixed(newShowFixed)
+      }
+      document.addEventListener('scroll', onScroll)
+
+      return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}> 
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        loading? <Item key='loading'><MdHourglassEmpty size='24px' /></Item>
+        : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <React.Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </React.Fragment>
   )
 }
